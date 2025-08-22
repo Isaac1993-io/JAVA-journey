e75c826f3e5428775431e7f68d047c8e4f44d785
isaac1993-io <isaacmogorlama@gmail.com>
2025-08-23 00:50:50 +0530

    Java Programs

---
+++ Arithmetic.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Arithmetic {
+    
+    public static void main(String[] args)
+    {
+        int x=10;
+        int y=5;
+
+        int add;
+        int sub;
+        int mul;
+        int div;
+
+        add= x + y;
+        sub= y - x;
+        mul= x * y;
+        div= x / y;
+    
+    System.out.println("add="+ add + "\n" +"sub =" + sub + "\n" + "mul =" + mul + "\n" + "div =" +div );
+}
+
+}
